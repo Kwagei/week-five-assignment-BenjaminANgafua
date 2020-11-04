4958dd65847de53060e3a96d3abf8b9423eb9a92
Benjamin A. Ngafua <benjaminarkutl2017@gmail.com>
2020-11-04 10:34:45 +0000

    Updated script

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var player = 100; //This is your limit of number you're login.
+var player = 100; //This is your limit of number you're logging in.
 var guessAmount = 10; //Number of time you can guess
 var count = 10; //In relation to the previous one, this will move in deccending order as a comes to zero game end.
-var magical = Math.floor(Math.random() *player); //This is the mathematical part(Math.floor(Math.random() with *player is not constant it can be substituded) that does the random selection for you in the background.
-//Below are the necessary condition for a best ou put.
+var magical = Math.floor(Math.random() *player); //This is the mathematical part(Math.floor(Math.random() with *player is not constant it can be substituded) that does the random selection for you in the background. That's the reason I give the property magical to it.
+//Below are the necessary condition for a best out put.
 while (Attempts != magical) { var Attempts = prompt("Choose any number from 1 to " + player);
     count -=1;
 
