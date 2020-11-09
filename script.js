66234b5b5975d75aae0cfb598bc50f196cbfc86b
Benjamin A. Ngafua <benjaminarkutl2017@gmail.com>
2020-11-09 23:24:28 +0000

    Completing

--- script.js
+++ script.js
@@ -1,22 +1,55 @@
-var player = 100; //A generated random number between 1 and 100.
-var guessAmount = 10; //Number of time you are to make a guess.
-var count = 10; //Number of chances left in the game for the player.
-var magical = Math.floor(Math.random() *player); //This is the mathematical part(Math.floor(Math.random() with *player is not constant it can be substituded) that does the random selection for you in the background. That's the reason I give the property magical to it.
-//Below are the necessary condition for a best out put.
-while (Attempts != magical) { var Attempts = prompt("Choose any number from 1 to " + player);
-    count -=1;
-    // The player guess is lower than the random number, "Too Low"
-    if (Attempts < magical) {document.write("Too Low");}
-    //The player guess is  higher than the random number, "Too High" . 
-    if (Attempts > magical) {document.write("Too High");}
-    //This will display number of chances left in the game for the player.
-    document.write("<p>" + count + " More attempt</p>");
-    // A player guess is the same as the random number, "You Win" and game end.
-    if (Attempts == magical){document.write("You Win");}
-    // A player failures to guess the number in 10 chances, "You Lose" and game end..
-    if (count < 1) {document.write("You Lose");}
+var btn_guide = document.getElementById("guide");
+var btn_begin = document.getElementById("begin");
+var btn_reset = document.getElementById("reset");
+var btn_check = document.getElementById("check");
 
-    if (count < 1) {document.write("<p> Answer " + magical + "</p>");
-    break}
+var main_div = document.getElementById("main-div");
+var guide_div = document.getElementById("guide-div")
+
+var guess_box = document.getElementById("guess-box");
+var guesses = document.getElementById("guesses");
+var high_or_low = document.getElementById("high-or-low");
+
+var random_num = Math.floor(Math.random()*100) + 1;
+var count_guess =1;
+
+function guide(){guide_div.style.visibility = "visible";}
+
+function begin(){main_div.style.visibility = "visible";
+
 }
-console.log("game complete")
+function checkGuess(){var your_guess = Number(guess_box.value);
+    if (count_guess <=10){ if (your_guess < random_num){
+        guesses.textContent += your_guess + " ";
+        document.getElementById("chances").innerHTML = "<div> Chances-attempted " + count_guess + "</div>";
+        high_or_low.textContent = "your guess is Low";
+        high_or_low.classList.add("wrong");
+        count_guess++;
+        guess_box.value = "";
+    }
+else if(your_guess > random_num){
+        guesses.textContent += your_guess + " ";
+        high_or_low.textContent = "your guess is High";
+        high_or_low.classList.add("wrong");
+        count_guess++;
+        guess_box.value = "";
+        }
+       
+else{   guesses.textContent += your_guess + " ";
+        high_or_low.textContent = "You win!";
+        high_or_low.classList.add("success");
+        guess_box.value = "";
+        gameOver();
+    }
+    }
+    else{ guesses.textContent += your_guess + " ";
+    high_or_low.textContent = "You Lose!";
+    high_or_low.classList.add("wrong");
+    guess_box.value = "";
+    gameOver();
+}
+}
+function gameOver(){
+    btn_check.disabled = true;
+    guess_box.disabled = true;
+}
