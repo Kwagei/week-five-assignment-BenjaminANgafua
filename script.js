df778259545ef6efaab1ce5745c24f774e9a6db3
benjaminangafua <benjaminarkutl2017@gmail.com>
2022-01-02 22:03:44 +0000

    Update input

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var btn_guide = document.getElementById("guide");
 var btn_begin = document.getElementsByClassName("begin-btn");
 var btn_check = document.getElementById("check");
 
+let container = document.getElementById("container")
 var guide_div = document.getElementById("guide-div")
 
 var guess_box = document.getElementById("guess-box");
@@ -17,46 +18,54 @@ var btn_hide = document.getElementById("hide");
 function checkGuess() {
     var your_guess = Number(guess_box.value);
 
-    if (count_guess > 1) {
-        count_guess--;
+    if (your_guess <= 100) {
+        if (count_guess > 1) {
+            count_guess--;
 
-        if (your_guess < random_num) {
+            if (your_guess < random_num) {
 
-            guesses.textContent += your_guess + ", ";
-            document.getElementById("chances").innerHTML = "<div> Chances-remain " + count_guess + "</div>";
-            high_or_low.textContent = "your guess is Low";
-            high_or_low.classList.add("wrong");
-            guess_box.value = "";
-        } else if (your_guess > random_num) {
-            guesses.textContent += your_guess + ", ";
-            high_or_low.textContent = "your guess is High";
-            document.getElementById("chances").innerHTML = "<div> Chances-remain " + count_guess + "</div>";
-            high_or_low.classList.add("wrong");
-            guess_box.value = ""; /*blank input field*/
+                guesses.textContent += your_guess + ", ";
+                document.getElementById("chances").innerHTML = "<div> Chances-remain " + count_guess + "</div>";
+                high_or_low.textContent = "your guess is Low";
+                high_or_low.classList.add("wrong");
+                guess_box.value = "";
+            } else if (your_guess > random_num) {
+                guesses.textContent += your_guess + ", ";
+                high_or_low.textContent = "your guess is High";
+                document.getElementById("chances").innerHTML = "<div> Chances-remain " + count_guess + "</div>";
+                high_or_low.classList.add("wrong");
+                guess_box.value = ""; /*blank input field*/
+            }
+
+            // if your guess is equal the random number value.
+            else {
+                guesses.textContent += your_guess + ", "; /*record of your guess*/
+                high_or_low.textContent = "You win!"; /*guess equal to the random-number number*/
+                high_or_low.classList.add("success"); /*Applied class created from stylesheet to your guess*/
+                guess_box.value = ""; /*blank input field*/
+                gameOver(); /*end the as you succeed*/
+            }
         }
-
-        // if your guess is equal the random number value.
+        // Chances over and your guess is not equal to the random-number number.
         else {
-            guesses.textContent += your_guess + ", "; /*record of your guess*/
-            high_or_low.textContent = "You win!"; /*guess equal to the random-number number*/
-            high_or_low.classList.add("success"); /*Applied class created from stylesheet to your guess*/
+            guesses.textContent += your_guess + " ";
+            high_or_low.textContent = "You Lose!"; /*guess not equal to the random-number number and chances over*/
+            high_or_low.classList.add("wrong"); /*Applied class created from stylesheet to your guess*/
             guess_box.value = ""; /*blank input field*/
-            gameOver(); /*end the as you succeed*/
+            gameOver(); /*end the as you lost*/
         }
-    }
-    // Chances over and your guess is not equal to the random-number number.
-    else {
-        guesses.textContent += your_guess + " ";
-        high_or_low.textContent = "You Lose!"; /*guess not equal to the random-number number and chances over*/
-        high_or_low.classList.add("wrong"); /*Applied class created from stylesheet to your guess*/
-        guess_box.value = ""; /*blank input field*/
-        gameOver(); /*end the as you lost*/
+    } else {
+        alert("Sorry out of range")
+        window.location.reload()
     }
 }
 
 function hide() { guide_div.style.display = "none"; }
 
-function guide() { guide_div.style.display = "block"; }
+function guide() {
+    guide_div.style.display = "block";
+
+}
 
 function hint() {
     let hint = document.getElementById("hint");
@@ -69,7 +78,7 @@ function hint() {
 }
 
 function begin() {
-    document.getElementById("container").style.display = "flex";
+    container.style.display = "flex";
     guide_div.style.display = "none"
 }
 //End game
